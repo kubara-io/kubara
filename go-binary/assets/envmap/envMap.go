5349c233d7e86200b6a0ae0c60493162af8a933c
Fabian <21021337+fabian-schmitt@users.noreply.github.com>
2026-04-17 15:03:00 +0200

    fix: outdated and confusing info in env file (#234)
    * fixed outdated and confusing info in env file.
    
    * Changed wording. Byebye Docker.

--- go-binary/assets/envmap/envMap.go
+++ go-binary/assets/envmap/envMap.go
@@ -33,9 +33,7 @@ type EnvMap struct {
 	_                           struct{} `doc:"\n### Project related values"`
 	ProjectName                 string   `default:"<...>" koanf:"PROJECT_NAME"`
 	ProjectStage                string   `default:"<...>" koanf:"PROJECT_STAGE"`
-	_                           struct{} `doc:"\n### Docker related values"`
-	_                           struct{} `doc:"# see https://docs.docker.com/reference/cli/docker/login/"`
-	_                           struct{} `doc:"# after successful login you can look inside envMap.json in your docker directory (~/.docker/envMap.json) on Linux/Mac"`
+	_                           struct{} `doc:"\n### Container Registry Config"`
 	_                           struct{} `doc:"# the variable must be base64 encoded - how to: https://docs.kubara.io/latest-stable/6_reference/faq/#how-do-i-create-a-dockerconfigjson-for-env-file"`
 	DockerconfigBase64          string   `default:"<...>" koanf:"DOCKERCONFIG_BASE64"`
 	_                           struct{} `doc:"\n### Argo CD related values"`
