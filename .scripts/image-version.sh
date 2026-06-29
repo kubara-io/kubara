cd48076e1f2f2819cf7bdeeb531534166b225325
davrad <david@darad.net>
2026-06-29 20:08:40 +0200

    feat: add files for the pipeline

--- .scripts/image-version.sh
+++ .scripts/image-version.sh
@@ -11,25 +11,23 @@ MANAGED="${MANAGED:-${PWD}/managed-service-catalog/helm}"
 PROFILES="${PROFILES:-${SCRIPT_DIR}/../.github/helm-profiles}"
 OUTPUT_FILE="${OUTPUT_FILE:-}"
 
-[[ -d "$MANAGED" ]] || { echo "::errror::MANAGED '$MANAGED' not found - run kubara generate first"; exit 1; }
+[[ -d "$MANAGED" ]] || { echo "::error::MANAGED '$MANAGED' not found - run kubara generate first"; exit 1; }
 [[ -d "$PROFILES" ]] || { echo "::error::PROFILES '$PROFILES' not found"; exit 1; }
 
-set -x
-
 render_all() {
-    echo "Running"
+    echo "Rendering..." >&2
     for chart_path in "$MANAGED"/*/; do
         [[ -f "$chart_path/Chart.yaml" ]] || continue
         chart=$(basename "$chart_path")
 
-        echo "Updating dependency for ${chart_path}"
+        echo "Updating dependency for ${chart_path}" >&2
 
         helm dependency update "$chart_path" >/dev/null 2>&1  || continue
 
         if [[ -d "$PROFILES/$chart" ]]; then
-            for profile in "$PROFILES/$chart/*.yaml"; do
+            for profile in "$PROFILES/$chart"/*.yaml; do
                 [[ -e "$profile" ]] || continue
-                helm template "$chart" "$chart_path" -f "profile" 2>/dev/null
+                helm template "$chart" "$chart_path" -f "$profile" 2>/dev/null
             done
         else
             helm template "$chart" "$chart_path" 2>/dev/null
@@ -37,18 +35,19 @@ render_all() {
     done
 }
 
-echo "Rendering templates"
+echo "Starting to render templates"
 
 IMAGES=$(
     render_all |
         grep '^\s*image:' |
         sed 's/^\s*image:\s*//' |
         tr -d '"' |
+        grep -v '[*!]' | # for kyverno images
         grep -v '^\s*$' |
         sort -u
 )
 
-echo "Done Rendering"
+echo "Done Rendering!"
 
 [[ -n "$IMAGES" ]] || { echo "::warning::No image references found"; exit 0; }
 
