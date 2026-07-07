add7bd8952587b866a3629e5498735bf1f2b4705
fabian-patrice.schmitt <21021337+fabian-schmitt@users.noreply.github.com>
2026-07-07 14:35:55 +0200

    feat: change dns name creation

--- src/internal/envconfig/env.go
+++ src/internal/envconfig/env.go
@@ -34,9 +34,7 @@ type EnvMap struct {
 	_                           struct{} `doc:"\n# Project related values"`
 	ProjectName                 string   `default:"<...>" koanf:"PROJECT_NAME"`
 	ProjectStage                string   `default:"<...>" koanf:"PROJECT_STAGE"`
-	_                           struct{} `doc:"\n# DNS Name/Zones related values"`
-	_                           struct{} `doc:"# The Domain name under which your dns-entries will be added."`
-	_                           struct{} `doc:"# The resulting dnsName name will be a concatenation of <PROJECT_NAME>.<DOMAIN_NAME> and will be used in clusters[].dnsName and clusters[].terraform.dns.name in config.yaml"`
+	_                           struct{} `doc:"\n# DNS Name/Zones related value"`
 	_                           struct{} `doc:"# the value should be looking like 'stackit.zone' eg. 'yourDomain.com'"`
 	DomainName                  string   `default:"<...>" koanf:"DOMAIN_NAME"`
 	_                           struct{} `doc:"\n# Argo CD related values"`
